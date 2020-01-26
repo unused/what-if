e31416611965bb29fd6f91bcb9c7516a0f392d81
Christoph Lipautz <christoph@lipautz.org>
2020-01-26 16:50:30 +0100

    Add production domain to known hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,4 +105,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # Allow hosts from
+  config.hosts << 'ifaas.herokuapp.com'
 end
